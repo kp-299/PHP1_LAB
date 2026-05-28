0d0c4d1a9c84ae3d2f530960d6b9e187aab99cf9
HieuPT <ngoxuanhieu2332005@gmail.com>
2026-05-28 18:37:36 +0700

    hiếu làm xong lab3

---
+++ lab3_Hieunx_PC11037/model/product.php
@@ -0,0 +1,19 @@
+<?php
+
+$products = [
+    [
+        "name" => "bưởi",
+        "price" => 35000,
+        "img" => "https://product.hstatic.net/200000157781/product/buoi_da_xanh_tan_trieu__4__e1fa8caf597c436e8f805ff7f61cdae3_1024x1024.png"
+    ],
+    [
+        "name" => "dưa hấu",
+        "price" => 39000,
+        "img" => "https://lh5.googleusercontent.com/V4hUu32b_EhY8ADSPDZeE9uaSSCE6eEdM6wZGj_ccUr7WR2zi5NIQ_FneYGYW4DjwtudjZIauTMZPswnwA_qx0THufW7lUFmRpycBnbt_viiv4B-AHtv-s7PckWFd7bd8SQQMzDXy9EpoiJq_0Y51MY"
+    ],
+    [
+        "name" => "ổi",
+        "price" => 18000,
+        "img" => "https://inkythuatso.com/uploads/thumbnails/800/2023/03/4-hinh-anh-qua-oi-inkythuatso-27-11-03-29.jpg"
+    ]
+];
